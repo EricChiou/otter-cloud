f0b3f55626ebafbb18df8ff3d77c2073daee67bd
EricChiou <miatlab@gmail.com>
2021-01-04 18:07:49 +0800

    tooltip preview image

---
+++ src/pages/Home/FileList/File/FileName/index.tsx
@@ -0,0 +1,94 @@
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import { ContentType } from 'src/constants/file';
+import { BaseTooltip } from 'src/components/common';
+import {
+  File as FileIcon, FileText, FileImage, FileAudio, FileVideo,
+  FileArchive, FilePdf, FileWord, FileExcel, FilePPT, FileFolder,
+  CheckBox,
+} from 'src/components/icons';
+import { File } from '../';
+import { getPreviewUrl } from 'src/api/file';
+import { selectPrefix } from 'src/store/system.slice';
+import { selectUserProfile } from 'src/store/user.slice';
+
+import styles from './style.module.scss';
+import table from '../../table.module.scss';
+
+interface Props {
+  file: File;
+  convertFileSize: () => string;
+  convertFileLastModified: () => string;
+}
+
+const FileName: FunctionComponent<Props> = ({ file, convertFileSize, convertFileLastModified }) => {
+  const userProfile = useSelector(selectUserProfile);
+  const prefix = useSelector(selectPrefix);
+
+  const renderIcon = () => {
+    let Icon = FileIcon;
+    if (file.contentType === '') {
+      Icon = FileFolder;
+    } else if (file.contentType.indexOf(ContentType.text) > -1) {
+      Icon = FileText;
+    } else if (file.contentType.indexOf(ContentType.image) > -1) {
+      Icon = FileImage;
+    } else if (file.contentType.indexOf(ContentType.audio) > -1) {
+      Icon = FileAudio;
+    } else if (file.contentType.indexOf(ContentType.video) > -1) {
+      Icon = FileVideo;
+    } else if (file.contentType.indexOf(ContentType.zip) > -1) {
+      Icon = FileArchive;
+    } else if (file.contentType.indexOf(ContentType.pdf) > -1) {
+      Icon = FilePdf;
+    } else if (file.contentType.indexOf(ContentType.word) > -1) {
+      Icon = FileWord;
+    } else if (file.contentType.indexOf(ContentType.excel) > -1) {
+      Icon = FileExcel;
+    } else if (file.contentType.indexOf(ContentType.ppt) > -1) {
+      Icon = FilePPT;
+    }
+
+    return <Icon></Icon>;
+  };
+
+  const renderTooltipContent = () => {
+    if (file.contentType.indexOf(ContentType.image) > -1) {
+      const PreviewImage: FunctionComponent<{}> = () => {
+        const [url, setUrl] = useState('');
+
+        useEffect(() => {
+          getPreviewUrl(prefix, file.name, userProfile.token).then((resp) => {
+            setUrl(resp.data.url);
+          });
+
+        }, []);
+
+        return <img className={styles.previewImage} src={url} alt="preview"></img>;
+      };
+
+      return <PreviewImage></PreviewImage>;
+    }
+
+    return file.name;
+  }
+
+  return (
+    <BaseTooltip content={renderTooltipContent()}>
+      {file.selected ?
+        <span className={styles.active}><CheckBox></CheckBox></span> :
+        <span>{renderIcon()}</span>
+      }
+      <span className={`${table.text} ${styles.text} ${styles.iconText}`}>
+        {file.name}
+        <div className={styles.subLine}>
+          {convertFileSize()}{file.contentType ? ', ' : null}
+          {convertFileLastModified()}
+        </div>
+      </span>
+    </BaseTooltip>
+  );
+};
+
+export default FileName;
--- src/pages/Home/FileList/File/index.tsx
+++ src/pages/Home/FileList/File/index.tsx
@@ -2,17 +2,12 @@ import React, { FunctionComponent } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 
-import { ContentType } from 'src/constants/file';
-import { BaseTooltip } from 'src/components/common';
-import {
-  File as FileIcon, FileText, FileImage, FileAudio, FileVideo,
-  FileArchive, FilePdf, FileWord, FileExcel, FilePPT, FileFolder,
-  Download, CheckBox,
-} from 'src/components/icons';
+import { Download, } from 'src/components/icons';
 import FileOptions from './FileOptions';
 import { selectPrefix } from 'src/store/system.slice';
 import { addTask } from 'src/shared/task-shared';
 import { TaskType, TaskStatus, TaskData } from 'src/components/TaskList/reducer';
+import FileName from './FileName';
 
 import styles from './style.module.scss';
 import table from '../table.module.scss';
@@ -90,49 +85,14 @@ const FileComponent: FunctionComponent<Props> = ({ file, index, onSelected }) =>
     addTask([task]);
   };
 
-  const renderIcon = () => {
-    let Icon = FileIcon;
-    if (file.contentType === '') {
-      Icon = FileFolder;
-    } else if (file.contentType.indexOf(ContentType.text) > -1) {
-      Icon = FileText;
-    } else if (file.contentType.indexOf(ContentType.image) > -1) {
-      Icon = FileImage;
-    } else if (file.contentType.indexOf(ContentType.audio) > -1) {
-      Icon = FileAudio;
-    } else if (file.contentType.indexOf(ContentType.video) > -1) {
-      Icon = FileVideo;
-    } else if (file.contentType.indexOf(ContentType.zip) > -1) {
-      Icon = FileArchive;
-    } else if (file.contentType.indexOf(ContentType.pdf) > -1) {
-      Icon = FilePdf;
-    } else if (file.contentType.indexOf(ContentType.word) > -1) {
-      Icon = FileWord;
-    } else if (file.contentType.indexOf(ContentType.excel) > -1) {
-      Icon = FileExcel;
-    } else if (file.contentType.indexOf(ContentType.ppt) > -1) {
-      Icon = FilePPT;
-    }
-
-    return <Icon></Icon>;
-  };
-
   return (
     <div className={`${table.file} ${styles.file}`}>
       <div className={table.nameCol} onClick={() => { onSelected(file, index); }}>
-        <BaseTooltip content={file.name}>
-          {file.selected ?
-            <span className={styles.active}><CheckBox></CheckBox></span> :
-            <span>{renderIcon()}</span>
-          }
-          <span className={`${table.text} ${styles.text} ${styles.iconText}`}>
-            {file.name}
-            <div className={styles.subLine}>
-              {convertFileSize()}{file.contentType ? ', ' : null}
-              {convertFileLastModified()}
-            </div>
-          </span>
-        </BaseTooltip>
+        <FileName
+          file={file}
+          convertFileSize={convertFileSize}
+          convertFileLastModified={convertFileLastModified}
+        ></FileName>
       </div>
       <div className={table.sizeCol}>
         <span className={table.text}>{convertFileSize()}</span>
