0cbc7379776ee07f30dfa3f594748ae998540322
EricChiou <miatlab@gmail.com>
2021-02-11 18:11:36 +0800

    fix router path

--- src/constants/routes-url.ts
+++ src/constants/routes-url.ts
@@ -1,6 +1,6 @@
 export class Routes {
-  public static readonly HOME = '/home';
-  public static readonly LOGIN = '/login';
-  public static readonly SIGN_UP = '/sign-up';
-  public static readonly SHARE_LINK = '/share-link';
+  public static readonly HOME = process.env.PUBLIC_URL + '/home';
+  public static readonly LOGIN = process.env.PUBLIC_URL + '/login';
+  public static readonly SIGN_UP = process.env.PUBLIC_URL + '/sign-up';
+  public static readonly SHARE_LINK = process.env.PUBLIC_URL + '/share-link';
 }
